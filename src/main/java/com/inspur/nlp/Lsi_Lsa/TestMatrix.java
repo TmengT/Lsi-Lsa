69a701ac1c523a3fcf2619fff320bec6f149620c
TmengT <474151727@qq.com>
2016-01-15 15:38:19 +0800

    潜在语义分析java实现
    潜在语义分析java实现

---
+++ src/main/java/com/inspur/nlp/Lsi_Lsa/TestMatrix.java
@@ -0,0 +1,10 @@
+package com.inspur.nlp.Lsi_Lsa;
+
+public class TestMatrix {    
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LsiLsa m = new LsiLsa(); 		
+		m.querySimilarity("入室 盗窃案 蓬莱市 实验中学 2002 年 10 月 12 日 凌晨 蓬莱市 实验中学 报警器 报警 有人 入室 盗窃");
+	}
+}
